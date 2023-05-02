af05200fa2f31315b52f6518fc8c40a66e486735
nova_44056 <senguy44056@gmail.com>
2023-05-01 21:49:59 -0500

    feature: sign-in-with-google

--- frontend/src/page/sign-in.js
+++ frontend/src/page/sign-in.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { auth } from "../firebase";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 
 function SignIn() {
   const [email, setEmail] = useState("");
@@ -19,32 +23,61 @@ function SignIn() {
       });
   };
 
+  const handleSignInWithGoogle = async (e) => {
+    e.preventDefault();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        console.log(user);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   return (
-    <form onSubmit={handleSignIn}>
-      <label htmlFor="email">Email:</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        autoComplete="username"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-      />
-      <br />
-      <label htmlFor="password">Password:</label>
-      <input
-        autoComplete="current-password"
-        id="password"
-        name="password"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <br />
-      <button type="submit">Sign In</button>
-    </form>
+    <>
+      <form onSubmit={handleSignIn}>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          autoComplete="username"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          autoComplete="current-password"
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <br />
+        <button type="submit">Sign In</button>
+      </form>
+      <button onClick={handleSignInWithGoogle}>Sign In with Google</button>
+    </>
   );
 }
 
