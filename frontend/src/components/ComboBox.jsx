4ba6aa948bb1936315cd798b8d6f06097532a9c0
Gavin Sidhu <gavinjeet.sidhu@gmail.com>
2023-05-03 14:03:01 -0700

    created select combobox for selecting friends

--- frontend/src/components/ComboBox.jsx
+++ frontend/src/components/ComboBox.jsx
@@ -21,9 +21,9 @@ export default function ComboBox({ friends }) {
       <div className="w-72">
         <Combobox multiple value={selected} onChange={setSelected}>
           <div className="relative mt-1">
-            <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md sm:text-sm">
               <Combobox.Input
-                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0 focus:outline-orange-300"
                 displayValue={(friends) => friends.name}
                 onChange={(event) => setQuery(event.target.value)}
               />
@@ -41,7 +41,7 @@ export default function ComboBox({ friends }) {
               leaveTo="opacity-0"
               afterLeave={() => setQuery("")}
             >
-              <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+              <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg focus:outline-none sm:text-sm">
                 {filteredfriends.length === 0 && query !== "" ? (
                   <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
                     Nothing found.
@@ -52,7 +52,7 @@ export default function ComboBox({ friends }) {
                       key={friends.id}
                       className={({ active }) =>
                         `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                          active ? "bg-teal-600 text-white" : "text-gray-900"
+                          active ? "bg-orange-600 text-white" : "text-gray-900"
                         }`
                       }
                       value={friends}
@@ -69,7 +69,7 @@ export default function ComboBox({ friends }) {
                           {selected ? (
                             <span
                               className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                active ? "text-white" : "text-teal-600"
+                                active ? "text-white" : "text-orange-600"
                               }`}
                             >
                               <HiCheck className="h-5 w-5" aria-hidden="true" />
