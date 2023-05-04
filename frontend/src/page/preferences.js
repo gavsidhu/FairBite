704ae8e9da4aaabcf37106f0d6d8dfdbdaa7fc7d
David <davidhariantowu@gmail.com>
2023-05-04 18:21:57 -0400

    Fixed preferences ui

--- frontend/src/page/preferences.js
+++ frontend/src/page/preferences.js
@@ -1,54 +1,87 @@
 import { CUISINE_TYPES } from "../constants";
 import React, { useState } from "react";
+import useAuth from "../hooks/useAuth"
 
 function Preferences() {
 
-    const[query, setQuery] = useState("");
     const [page, setPage] = useState(1);
+    const [selectedCuisineIndexes, setSelectedCuisineIndexes] = useState([]);
     const itemsPerPage = 40;
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    const filteredCuisines = Object.values(CUISINE_TYPES).filter((cuisine) =>
-    cuisine.toLowerCase().startsWith(query.toLowerCase()));
-    const cuisinesOnPage = filteredCuisines.slice(startIndex, endIndex);
-      
-    //const [imageURL, setImageURL] = useState(null);
-    //const REACT_APP_PIXABAY_API_KEY="35999918-2bb0aa9d0088e12e2a728fd1a"
+    const allCuisines = Object.values(CUISINE_TYPES);
+    const cuisinesOnPage = allCuisines.slice(startIndex, endIndex);
+
+    const handleCheckboxChange = (index) => {
+        setSelectedCuisineIndexes((prevSelectedCuisineIndexes) => {
+        if (prevSelectedCuisineIndexes.includes(index)) {
+            return prevSelectedCuisineIndexes.filter((i) => i !== index);
+        } else if (prevSelectedCuisineIndexes.length < 5) {
+            return [...prevSelectedCuisineIndexes, index];
+        } else {
+            return prevSelectedCuisineIndexes;
+        }
+        });
+    };
 
     return(
         <div className="flex flex-col items-center justify-center mb-10">
-            <h1 className="mb-10 mt-10 ml-10 text-5xl font-bold">Welcome Team6</h1> {/* Welcome {username} */}
+            <h1 className="mb-10 mt-10 ml-10 text-5xl font-bold">Welcome</h1> {/* Welcome {username} */}
             <h2 className="text-2xl">What do you want to eat?</h2>
-            <h2>Choose up to 5 cuisines to personalize your FairBite experience</h2>
-            <div className="flex flex-row items-center space-x-4">
-                <input className="mb-5 mt-5 px-4 py-2 border border-gray-300 rounded-lg w-80" placeholder="" onChange={event => setQuery(event.target.value)} />
-                <img src="https://cdn-icons-png.flaticon.com/512/149/149852.png" alt="Search icon" class="w-6 h-6"></img>
-            </div>
+            <h2 className="mb-5">Choose 3 to 5 cuisines to personalize your FairBite experience</h2>
+            <h2 className="text-2xl font-bold text-green-600">
+                {selectedCuisineIndexes.length < 3 && <span className="text-red-600">
+                    {selectedCuisineIndexes.length} / 5 selected
+                </span>}
+                {selectedCuisineIndexes.length >= 3 && <span className="text-green-600">
+                    {selectedCuisineIndexes.length} / 5 selected
+                </span>}
+            </h2>
             <div className="w-4/5">
                 <ul class="grid grid-cols-4 gap-4 mt-4 mb-10">
-                    {cuisinesOnPage.map((cuisine) => (
-                        <li class="flex items-center space-x-2">
+                    {cuisinesOnPage.map((cuisine, index) => (
+                        <li class="flex items-center space-x-2" >
                             <input 
+                                id={`cuisine-${startIndex + index}`}
                                 type="checkbox" 
+                                style={{ cursor: "pointer" }}
+                                className="w-5 h-5 "
+                                checked={selectedCuisineIndexes.includes(allCuisines.indexOf(cuisine))}
+                                onChange={() => handleCheckboxChange(allCuisines.indexOf(cuisine))}
                             />
-                            <span className="text-2xl">{cuisine}</span>
+                            <label htmlFor={`cuisine-${startIndex + index}`} style={{ cursor: "pointer", userSelect: "none" }} className="text-2xl">{cuisine}</label>
                         </li>
                     ))}
                 </ul>
-                <div className="flex flex-row items-center justify-center space-x-4 fixed bottom-10 left-1/2 transform -translate-x-1/2">
-                    <button 
-                        disabled={page === 1} 
-                        onClick={() => setPage(page - 1)} 
-                        className="px-4 py-2 bg-orange-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 mr-2">
-                        Previous
-                    </button>
-                    <p className="font-bold text-lg">{page}/{Math.ceil(filteredCuisines.length/itemsPerPage)}</p>
-                    <button 
-                        disabled={endIndex >= filteredCuisines.length} 
-                        onClick={() => setPage(page + 1)} 
-                        className="px-4 py-2 bg-orange-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700">
-                        Next
-                    </button>
+                <div>
+                    {/* This displays the selected checkboxes */}
+                    {selectedCuisineIndexes.map((index) => Object.values(CUISINE_TYPES)[index])}
+                </div>
+                <div>
+
+                </div>
+                <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2">
+                    <div className="flex items-center justify-center mb-8">
+                        <button
+                            className="px-4 py-2 bg-green-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700">
+                            Submit
+                        </button>
+                    </div>
+                    <div className="flex flex-row items-center justify-center space-x-4">
+                        <button 
+                            disabled={page === 1} 
+                            onClick={() => setPage(page - 1)} 
+                            className="px-4 py-2 bg-orange-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 mr-2">
+                            Previous
+                        </button>
+                        <p className="font-bold text-lg">{page}/{Math.ceil(allCuisines.length/itemsPerPage)}</p>
+                        <button 
+                            disabled={endIndex >= allCuisines.length} 
+                            onClick={() => setPage(page + 1)} 
+                            className="px-4 py-2 bg-orange-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700">
+                            Next
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
